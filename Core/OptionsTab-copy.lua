54098bb1243e2d5541a8ae60b26f714956b04daa
Niketa <niketa.wow@gmail.com>
2022-05-16 02:38:52 -0500

    Reset OptionsFrame

--- Core/OptionsTab-copy.lua
+++ Core/OptionsTab-copy.lua
@@ -1,3 +1,11 @@
 local addonName = ...
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
+
+addon.GetOptionsTab_Args1 = function()
+	return {}
+end
+
+addon.GetOptionsTab_Args2 = function()
+	return {}
+end
